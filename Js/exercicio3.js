028434a36242469ee2fe342880637ebfc95b7f5d
ribeirodonascimento <121270162+ribeirodonascimento@users.noreply.github.com>
2024-10-30 19:27:30 -0300

    Update exercicio3.js

--- Js/exercicio3.js
+++ Js/exercicio3.js
@@ -39,3 +39,14 @@ for(i=0; i < 5; i++){
  if (i === 5) {
   alert("Excedeu o limite de tentativas.");
 }
+
+
+
+4- Faça um programa que leia e valide as seguintes informações:
+Nome: maior que 3 caracteres;
+Idade: entre 0 e 150;
+Salário: maior que zero;
+Sexo: 'f' ou 'm';
+Estado Civil: 's', 'c', 'v', 'd'.
+Dica: se sua variável é texto, o tamanho dela está armazenado em: texto.length
+
