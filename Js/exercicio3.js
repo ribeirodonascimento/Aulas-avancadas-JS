38efe5a1912f2ff4a43b0d95aab3c58735217683
ribeirodonascimento <121270162+ribeirodonascimento@users.noreply.github.com>
2024-11-04 18:29:16 -0300

    Update exercicio3.js

--- Js/exercicio3.js
+++ Js/exercicio3.js
@@ -178,3 +178,6 @@ document.write(`A soma dos dois numeros é : ${soma}`)
 }
 
   
+14- Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número. Não utilize a função de potência da linguagem.
+
+  
